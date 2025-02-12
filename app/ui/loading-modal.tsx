2c73e723f115c6c366d1fe6ba453d7728d95b57f
CptnPluto <cptnpluto@gmail.com>
2025-02-12 17:11:13 +0200

    Loading modal update.

--- app/ui/loading-modal.tsx
+++ app/ui/loading-modal.tsx
@@ -7,15 +7,16 @@ export default function LoadingModal({ isPending }: { isPending: boolean }) {
 			id="popup-modal"
 			tabIndex={-1}
 			className={clsx(
-				"flex top-0 right-0 left-0 z-50 justify-center items-center w-full fixed h-screen max-h-full bg-gray-600/50",
-				{ hidden: !isPending }
+				"flex top-0 right-0 left-0 z-50 justify-center items-center w-full fixed h-screen max-h-full bg-gray-600/90",
+				
 			)}
 		>
 			<div className="relative p-4 w-full max-w-md max-h-full flex items-center justify-center gap-2">
 				<div className="flex items-center justify-center rounded-4xl">
-					<p className="absolute text-4xl font-bold animate-bounce bg-blue-300/50 rounded-4xl p-3">
-						Loading...
-					</p>
+                    
+                    <p className="absolute text-4xl font-bold animate-bounce bg-blue-300/50 rounded-full p-6 w-48 h-48 flex items-center justify-center">
+                        Loading...
+                    </p>
 				</div>
 			</div>
 		</div>
