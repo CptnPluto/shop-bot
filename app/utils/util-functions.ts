49b2e0d7341944c86696373f29f74c253d3a5691
CptnPluto <cptnpluto@gmail.com>
2025-02-16 20:21:22 +0200

    .

--- app/utils/util-functions.ts
+++ app/utils/util-functions.ts
@@ -1,7 +1,5 @@
 export function getBaseUrl() {
-    return process.env.VERCEL_ENV === "production"
-      ? `https://www.my-custom-domain.com`
-      : process.env.VERCEL_URL
+    return process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : `http://localhost:3000`;
   }
